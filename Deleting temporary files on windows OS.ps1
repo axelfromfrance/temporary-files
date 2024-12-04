e8b2dc1f12066ab28a5511ab6f7ad3d935a69df3
Axel <113134516+axelfromfrance@users.noreply.github.com>
2024-12-04 14:55:13 +0100

    Add files via upload

---
+++ Deleting temporary files on windows OS.ps1
@@ -0,0 +1,38 @@
+﻿# Temporary File Path
+$TempUser = "$env:LOCALAPPDATA\Temp"
+$TempSystem = "$env:SystemRoot\Temp"
+
+# Function to securely delete files and folders
+function Clear-TemporaryFiles {
+    param (
+        [string]$Path
+    )
+    Write-Host "Nettoyage du dossier : $Path" -ForegroundColor Yellow
+
+    # Check if the path exists
+    if (Test-Path $Path) {
+        try {
+            # Delete the files
+            Get-ChildItem -Path $Path -File -Force -ErrorAction SilentlyContinue | ForEach-Object {
+                Remove-Item $_.FullName -Force -ErrorAction SilentlyContinue
+            }
+
+            # delete the folders
+            Get-ChildItem -Path $Path -Directory -Force -ErrorAction SilentlyContinue | ForEach-Object {
+                Remove-Item $_.FullName -Recurse -Force -ErrorAction SilentlyContinue
+            }
+
+            Write-Host "Cleaning completed for : $Path" -ForegroundColor Green
+        } catch {
+            Write-Host "Error while cleaning : $Path. Message : $_" -ForegroundColor Red
+        }
+    } else {
+        Write-Host "The path $Path doesn't exist." -ForegroundColor Red
+    }
+}
+
+# Cleaning user and system temporary files
+Clear-TemporaryFiles -Path $TempUser
+Clear-TemporaryFiles -Path $TempSystem
+
+Write-Host "Nettoyage terminé pour tous les répertoires temporaires." -ForegroundColor Cyan
